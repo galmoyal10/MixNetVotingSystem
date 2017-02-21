18789cd362d3c3c58a7f81077f7c968846ddd44e
Eran Goldstein <eran306gold@gmail.com>
2017-02-21 20:24:56 +0200

    Added documentation to the verifier classes

--- zkproof/fs_hueristics/fs_verifier.py
+++ zkproof/fs_hueristics/fs_verifier.py
@@ -3,28 +3,48 @@ import hashlib as hl
 
 
 class FsSwitchVerifier:
+    """
+    A non-interactive implementation of a verifier for a single mix-network switch, using Fiat-Shamir hueristics.
+    """
     def __init__(self, generator, q, y):
-        # Group used in the algorithm
+        """
+        :param generator: Generator of the group
+        :param q: Order of group Z_q used in the algorithm
+        :param y: Prover's public key
+        :param challenge_func: Function used for calculating the challenge
+        """
         self.q = q
-
-        # Prover's public key
         self.y = y
-
-        # Generator of the group
         self.g = generator
 
         # Interactive sigma-protocol verifier
         self.verifier = SwitchVerifier(generator, q, y, FsSwitchVerifier.getChallenge)
 
-
-    # Given the prover's response to the challenge, verify the proof's validity
-    # e - array of length 2
-    # z - 2x2 matrix
     def verify(self, message, T, W, e, z, in_m, in_g, out_m, out_g):
+        """
+        Given the switch's inputs and outputs, and the ZK-proof related to it (as described in the article),
+        check it's validity.
+        :param message: the first-message protobuf object (used to create the challenge).
+        :param T: 2x2 Matrix T_{b,i}, containing the inverse value from the value described in the article.
+        :param W: 2x2 Matrix W_{b,i}, containing the inverse value from the value described in the article.
+        :param e: 2-sized array [e_0, e_1] containing the challenges s.t. e_0 + e_1 = c, where c is the challenge
+        dictated by the sigma protocol.
+        :param z: 2x2 matrix z_{b,i}, as described in the article (contains the DLog challenges)
+        :param in_m: Inputs M_0, M_1 of the switch, as described in the article.
+        :param in_g: Inputs G_0, G_1 of the switch, as described in the article.
+        :param out_m: Outputs M^_0, M^_1 of the switch, as described in the article.
+        :param out_g: Outputs G^_0, G^_1 of the switch, as described in the article.
+        :return: whether the given proof is valid or not
+        """
         self.verifier.challenge(message)
         return self.verifier.verify(T, W, e, z, in_m, in_g, out_m, out_g)
 
     # Create a challenge based on T, W
     @staticmethod
     def getChallenge(message):
+        """
+        Create a challenge, derived by the input message
+        :param message:
+        :return: a challenge derived from the input message.
+        """
         return int(hl.sha256(message).hexdigest(), 16)
--- zkproof/verifier.py
+++ zkproof/verifier.py
@@ -2,37 +2,53 @@ from secure_random import SecureRandom
 
 
 class SwitchVerifier:
+    """
+    An interactive implementation of a verifier for a single mix-network switch
+    """
     def __init__(self, generator, q, y, challenge_func):
-        # Switch-Gate inputs
+        """
+        :param generator: Generator of the group
+        :param q: Order of group Z_q used in the algorithm
+        :param y: Prover's public key
+        :param challenge_func: Function used for calculating the challenge
+        """
 
-        # Group used in the algorithm
         self.q = q
-
-        # Prover's public key
         self.y = y
-
-        # Generator of the group
         self.g = generator
-
         self._challenge_func = challenge_func
 
-    # Return a challenge chosen at random, given the commitment of the prover
     def challenge(self, message):
+        """
+        Return a challenge chosen at random, given the commitment of the prover
+        :param message: input message
+        :return: challenge derived from the input message
+        """
         # Generate a challenge c from Z_q
         self._c = self._challenge_func(message)
 
         return self._c
 
-    # Given the prover's response to the given challenge, verify the proof's validity
-    # e - array of length 2
-    # z - 2x2 matrix
     def verify(self, T, W, e, z, in_m, in_g, out_m, out_g):
-
+        """
+        Given the switch's inputs and outputs, and the ZK-proof related to it (as described in the article),
+        check it's validity.
+        :param T: 2x2 Matrix T_{b,i}, containing the inverse value from the value described in the article.
+        :param W: 2x2 Matrix W_{b,i}, containing the inverse value from the value described in the article.
+        :param e: 2-sized array [e_0, e_1] containing the challenges s.t. e_0 + e_1 = c, where c is the challenge
+        dictated by the sigma protocol.
+        :param z: 2x2 matrix z_{b,i}, as described in the article (contains the DLog challenges)
+        :param in_m: Inputs M_0, M_1 of the switch, as described in the article.
+        :param in_g: Inputs G_0, G_1 of the switch, as described in the article.
+        :param out_m: Outputs M^_0, M^_1 of the switch, as described in the article.
+        :param out_g: Outputs G^_0, G^_1 of the switch, as described in the article.
+        :return: whether the given proof is valid or not
+        """
         # Validation check for e0 + e1 = c (mod q)
         if (self._c % self.q) != ((e[0] + e[1]) % self.q):
             return False
 
-        # Other checks validating the correctness of T_[b,i] and W_[b,i], for some b,i
+        # Other checks validating the correctness of T_[b,i] and W_[b,i], for all b,i
         for b in xrange(0, 2):
             for i in xrange(0, 2):
                 b_xor_i = b ^ i
@@ -50,8 +66,13 @@ class SwitchVerifier:
                     return False
         return True
 
-    # Create a challenge based on T, W
     @staticmethod
     def generate_challenge(w_matrix, t_matrix):
+        """
+        Generates a challenge according the given input matrices. (default method)
+        :param w_matrix:
+        :param t_matrix:
+        :return: challenge derived from the input matrices.
+        """
         rand = SecureRandom()
         return rand.getrandbits(len(t_matrix) + len(w_matrix))
