7977decbacad8deb2e8635fb56a27ce86f193363
Yarden Moskovich <mosko169@gmail.com>
2017-02-22 23:19:00 +0200

    fixed curves support bug

--- group_arithmetics/elliptic_curve_group.py
+++ group_arithmetics/elliptic_curve_group.py
@@ -6,8 +6,9 @@ from copy import deepcopy
 from asn1crypto.keys import ECPoint
 
 class EcException(Exception):
-    def __init__(self):
-        self.message = "Invalid elliptic curve point"
+    def __init__(self, err_info):
+        self.message = "Elliptic Curve Exception occurred: \n" \
+                       "{}".format(err_info)
 
     def __str__(self):
         return self.message
@@ -26,12 +27,10 @@ class EllipticCurveGroup(MultiplicativeGroup):
         """
         :return:(p,q,g) such as p,q large primes p = 2q+1
         """
-
         field = reg.get_curve(curve_name).field
         ec_order = field.n
-
-        return (2*ec_order+1, ec_order, EllipticCurvePoint.from_coords(EllipticCurveGroup.CURVE_NAME,
-                                       *field.g))
+        return (2*ec_order+1, ec_order, EllipticCurvePoint.from_coords(curve_name,
+                                                                       *field.g))
 
 
 class EllipticCurvePoint(MultiplicativeGroupItem):
@@ -42,7 +41,11 @@ class EllipticCurvePoint(MultiplicativeGroupItem):
         if kwargs.has_key('point') and (type(kwargs['point']) is ec.Point or type(kwargs['point']) is ec.Inf):
             self._ec_point = deepcopy(kwargs['point'])
         elif kwargs.has_key('curve_name') and kwargs.has_key('x_coord') and kwargs.has_key('y_coord'):
-            self._ec_point = ec.Point(reg.get_curve(kwargs['curve_name']), kwargs['x_coord'], kwargs['y_coord'])
+            try:
+                self._ec_point = ec.Point(reg.get_curve(kwargs['curve_name']), kwargs['x_coord'], kwargs['y_coord'])
+            except ValueError:
+                raise EcException("unsupported elliptic curve - {}\n"
+                              "supported curves: {}".format(kwargs['curve_name'], reg.EC_CURVE_REGISTRY.keys()))
         if not self._ec_point.on_curve:
             raise EcException
 
@@ -58,23 +61,17 @@ class EllipticCurvePoint(MultiplicativeGroupItem):
         """
         Creates an elliptic curve point from asn1 encoded bytestring, which may be compressed or uncompressed.
         """
-
         # The point is not compressed
         if asn1_encoded_bytestring[0] == '\x04':
             asn1_ec = ECPoint.load(asn1_encoded_bytestring)
             x, y = asn1_ec.to_coords()
             return EllipticCurvePoint(curve_name=curve_name, x_coord=x, y_coord=y)
 
-        # The point is not compressed
+        # The point is compressed
         elif asn1_encoded_bytestring[0] == '\x02' or asn1_encoded_bytestring[0] == '\x03':
-            key_bytes_len = len(asn1_encoded_bytestring)
-
             key_type = asn1_encoded_bytestring[0]
 
-            if key_bytes_len != 33:
-                raise ValueError("key_bytes must be exactly 33 bytes long when compressed.")
-
-            x = int(asn1_encoded_bytestring[1:33].encode("hex"), 16)
+            x = int(asn1_encoded_bytestring[1:].encode("hex"), 16)
             ys = EllipticCurvePoint._y_from_x(x, curve_name)
 
             # Pick the one that corresponds to key_type
@@ -86,19 +83,80 @@ class EllipticCurvePoint(MultiplicativeGroupItem):
             return EllipticCurvePoint(curve_name=curve_name, x_coord=x, y_coord=y)
         # Unsupported ec type.
         else:
-            raise EcException()
+            raise EcException("could not decode point")
 
     @staticmethod
-    def _modsqrt(a, n):
+    def legendre_symbol(a, p):
         """
-        utility function for calculating modulo sqrt
+        taken from http://codereview.stackexchange.com/questions/43210/tonelli-shanks-algorithm-implementation-of-prime-modular-square-root
+        Legendre symbol
+        Define if a is a quadratic residue modulo odd prime
+        http://en.wikipedia.org/wiki/Legendre_symbol
         """
+        ls = pow(a, (p - 1) / 2, p)
+        if ls == p - 1:
+            return -1
+        return ls
+
+    @staticmethod
+    def _modsqrt(a, p):
+        """
+        taken from http://codereview.stackexchange.com/questions/43210/tonelli-shanks-algorithm-implementation-of-prime-modular-square-root
+        Square root modulo prime number
+        Solve the equation
+            x^2 = a mod p
+        and return list of x solution
+        http://en.wikipedia.org/wiki/Tonelli-Shanks_algorithm
+        """
+        a %= p
+
+        # Simple case
         if a == 0:
-            return 0
-        elif n == 2:
-            return n
-        elif n % 4 == 3:
-            return pow(a, (n + 1) // 4, n)
+            return [0]
+        if p == 2:
+            return [a]
+
+        # Check solution existence on odd prime
+        if EllipticCurvePoint.legendre_symbol(a, p) != 1:
+            return []
+
+        # Simple case
+        if p % 4 == 3:
+            x = pow(a, (p + 1) / 4, p)
+            return [x, p - x]
+
+        # Factor p-1 on the form q * 2^s (with Q odd)
+        q, s = p - 1, 0
+        while q % 2 == 0:
+            s += 1
+            q //= 2
+
+        # Select a z which is a quadratic non resudue modulo p
+        z = 1
+        while EllipticCurvePoint.legendre_symbol(z, p) != -1:
+            z += 1
+        c = pow(z, q, p)
+
+        # Search for a solution
+        x = pow(a, (q + 1) / 2, p)
+        t = pow(a, q, p)
+        m = s
+        while t != 1:
+            # Find the lowest i such that t^(2^i) = 1
+            i, e = 0, 2
+            for i in xrange(1, m):
+                if pow(t, e, p) == 1:
+                    break
+                e *= 2
+
+            # Update next value to iterate
+            b = pow(c, 2 ** (m - i - 1), p)
+            x = (x * b) % p
+            t = (t * b * b) % p
+            c = (b * b) % p
+            m = i
+
+        return [x, p - x]
 
     @staticmethod
     def _y_from_x(x, curve_name):
@@ -107,8 +165,9 @@ class EllipticCurvePoint(MultiplicativeGroupItem):
         """
         curve = reg.get_curve(curve_name)
         a = (pow(x, 3, curve.field.p) + curve.a * x + curve.b) % curve.field.p
-        y1 = EllipticCurvePoint._modsqrt(a, curve.field.p)
-        y2 = curve.field.p - y1
+        y1, y2 = EllipticCurvePoint._modsqrt(a, curve.field.p)
+        if y1 is None:
+            raise EcException("could not get y from x")
         rv = []
 
         if EllipticCurvePoint(curve_name=curve_name, x_coord=x, y_coord=y1).on_curve:
